1a69344e7457688654181a4e34ab45ac55c07a65
J-O12 <joeloike@gmail.com>
2025-02-06 00:55:43 +1000

    recipes6

--- Terraform/dynamo.tf
+++ Terraform/dynamo.tf
@@ -1,15 +1,15 @@
-resource "aws_dynamodb_table" "RecipesTable" {
-  name         = var.RecipesTableName
-  billing_mode = "PAY_PER_REQUEST"
+# resource "aws_dynamodb_table" "RecipesTable" {
+#   name         = var.RecipesTableName
+#   billing_mode = "PAY_PER_REQUEST"
 
-  attribute {
-    name = "id"
-    type = "S"
-  }
+#   attribute {
+#     name = "id"
+#     type = "S"
+#   }
 
-  hash_key = "id"
-}
+#   hash_key = "id"
+# }
 
-output "RecipesTableName" {
-  value = aws_dynamodb_table.RecipesTable.name
-}
+# output "RecipesTableName" {
+#   value = aws_dynamodb_table.RecipesTable.name
+# }
