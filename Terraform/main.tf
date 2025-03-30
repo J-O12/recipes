380c48561ffeab4d67dc93d7d958bc951eb2f591
J-O12 <joeloike@gmail.com>
2025-03-31 00:52:24 +1000

    recipe_websites

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,140 +1,140 @@
-resource "aws_s3_bucket" "recipe-static-website-joel" {
-  bucket = "recipe-static-website-joel"
+# resource "aws_s3_bucket" "recipe-static-website-joel" {
+#   bucket = "recipe-static-website-joel"
 
-  tags = {
-    Name = "Test bucket"
-  }
-}
+#   tags = {
+#     Name = "Test bucket"
+#   }
+# }
 
-resource "aws_s3_bucket_website_configuration" "website-config" {
-  bucket = aws_s3_bucket.recipe-static-website-joel.id
+# resource "aws_s3_bucket_website_configuration" "website-config" {
+#   bucket = aws_s3_bucket.recipe-static-website-joel.id
 
-  index_document {
-    suffix = "index.html"
-  }
-}
+#   index_document {
+#     suffix = "index.html"
+#   }
+# }
 
-resource "aws_s3_bucket_ownership_controls" "recipe-controls" {
-  bucket = aws_s3_bucket.recipe-static-website-joel.id
-  rule {
-    object_ownership = "BucketOwnerEnforced"
-  }
-}
+# resource "aws_s3_bucket_ownership_controls" "recipe-controls" {
+#   bucket = aws_s3_bucket.recipe-static-website-joel.id
+#   rule {
+#     object_ownership = "BucketOwnerEnforced"
+#   }
+# }
 
-resource "aws_s3_bucket_public_access_block" "recipe-access" {
-  bucket = aws_s3_bucket.recipe-static-website-joel.id
+# resource "aws_s3_bucket_public_access_block" "recipe-access" {
+#   bucket = aws_s3_bucket.recipe-static-website-joel.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
 
-resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.recipe-static-website-joel.id
-  versioning_configuration {
-    status = "Disabled"
-  }
-}
+# resource "aws_s3_bucket_versioning" "versioning_example" {
+#   bucket = aws_s3_bucket.recipe-static-website-joel.id
+#   versioning_configuration {
+#     status = "Disabled"
+#   }
+# }
 
-data "aws_caller_identity" "current" {}
+# data "aws_caller_identity" "current" {}
 
-resource "aws_s3_bucket_policy" "recipe_policy" {
-  bucket = aws_s3_bucket.recipe-static-website-joel.id
+# resource "aws_s3_bucket_policy" "recipe_policy" {
+#   bucket = aws_s3_bucket.recipe-static-website-joel.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "AllowCloudFrontAccess"
-        Effect = "Allow"
-        Principal = {
-          Service = "cloudfront.amazonaws.com"
-        }
-        Action   = "s3:GetObject"
-        Resource = "${aws_s3_bucket.recipe-static-website-joel.arn}/*"
-        Condition = {
-          StringEquals = {
-            "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.s3_distribution.id}"
-          }
-        }
-      }
-    ]
-  })
-}
-
-
-# cloudfront
-resource "aws_cloudfront_origin_access_control" "recipe-origin" {
-  name                              = "recipe-OAC"
-  description                       = "recipe Policy"
-  origin_access_control_origin_type = "s3"
-  signing_behavior                  = "always"
-  signing_protocol                  = "sigv4"
-}
-
-locals {
-  s3_origin_id = "RecipeOrigin"
-}
-
-data "aws_cloudfront_cache_policy" "recipe-cache-policy" {
-  name = "Managed-CachingOptimized"
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Sid    = "AllowCloudFrontAccess"
+#         Effect = "Allow"
+#         Principal = {
+#           Service = "cloudfront.amazonaws.com"
+#         }
+#         Action   = "s3:GetObject"
+#         Resource = "${aws_s3_bucket.recipe-static-website-joel.arn}/*"
+#         Condition = {
+#           StringEquals = {
+#             "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.s3_distribution.id}"
+#           }
+#         }
+#       }
+#     ]
+#   })
+# }
 
 
-resource "aws_cloudfront_distribution" "s3_distribution" {
-  origin {
-    domain_name              = aws_s3_bucket.recipe-static-website-joel.bucket_regional_domain_name
-    origin_access_control_id = aws_cloudfront_origin_access_control.recipe-origin.id
-    origin_id                = local.s3_origin_id
+# # cloudfront
+# resource "aws_cloudfront_origin_access_control" "recipe-origin" {
+#   name                              = "recipe-OAC"
+#   description                       = "recipe Policy"
+#   origin_access_control_origin_type = "s3"
+#   signing_behavior                  = "always"
+#   signing_protocol                  = "sigv4"
+# }
 
-  }
+# locals {
+#   s3_origin_id = "RecipeOrigin"
+# }
 
-  enabled         = true
-  is_ipv6_enabled = true
-  default_root_object = "index.html"
+# data "aws_cloudfront_cache_policy" "recipe-cache-policy" {
+#   name = "Managed-CachingOptimized"
+# }
 
 
+# resource "aws_cloudfront_distribution" "s3_distribution" {
+#   origin {
+#     domain_name              = aws_s3_bucket.recipe-static-website-joel.bucket_regional_domain_name
+#     origin_access_control_id = aws_cloudfront_origin_access_control.recipe-origin.id
+#     origin_id                = local.s3_origin_id
 
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
-    cache_policy_id  = data.aws_cloudfront_cache_policy.recipe-cache-policy.id
+#   }
+
+#   enabled         = true
+#   is_ipv6_enabled = true
+#   default_root_object = "index.html"
 
 
 
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-
-
-  price_class = "PriceClass_100"
-
-  http_version = "http2"
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-      #   locations        = ["US", "CA", "GB", "DE"]
-    }
-  }
-  custom_error_response {
-    error_caching_min_ttl = 0
-    error_code            = 403
-    response_code         = 200
-    response_page_path    = "/"
-  }
+#   default_cache_behavior {
+#     allowed_methods  = ["GET", "HEAD"]
+#     cached_methods   = ["GET", "HEAD"]
+#     target_origin_id = local.s3_origin_id
+#     cache_policy_id  = data.aws_cloudfront_cache_policy.recipe-cache-policy.id
 
 
 
-  viewer_certificate {
-    # cloudfront_default_certificate = true
-    acm_certificate_arn = var.certificate-arn
-    ssl_support_method  = "sni-only"
-  }
+#     viewer_protocol_policy = "redirect-to-https"
+#     min_ttl                = 0
+#     default_ttl            = 3600
+#     max_ttl                = 86400
+#   }
 
-}
+
+#   price_class = "PriceClass_100"
+
+#   http_version = "http2"
+
+#   restrictions {
+#     geo_restriction {
+#       restriction_type = "none"
+#       #   locations        = ["US", "CA", "GB", "DE"]
+#     }
+#   }
+#   custom_error_response {
+#     error_caching_min_ttl = 0
+#     error_code            = 403
+#     response_code         = 200
+#     response_page_path    = "/"
+#   }
+
+
+
+#   viewer_certificate {
+#     # cloudfront_default_certificate = true
+#     acm_certificate_arn = var.certificate-arn
+#     ssl_support_method  = "sni-only"
+#   }
+
+# }
 
--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -1,108 +1,108 @@
-resource "aws_vpc" "recipe" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = "true"
-  enable_dns_support   = "true"
-}
+# resource "aws_vpc" "recipe" {
+#   cidr_block           = "10.0.0.0/16"
+#   enable_dns_hostnames = "true"
+#   enable_dns_support   = "true"
+# }
 
-resource "aws_subnet" "public-sub-1" {
-  vpc_id                  = aws_vpc.recipe.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-southeast-2a"
-  map_public_ip_on_launch = var.map_public_ip_on_launch
-}
+# resource "aws_subnet" "public-sub-1" {
+#   vpc_id                  = aws_vpc.recipe.id
+#   cidr_block              = "10.0.1.0/24"
+#   availability_zone       = "ap-southeast-2a"
+#   map_public_ip_on_launch = var.map_public_ip_on_launch
+# }
 
-resource "aws_subnet" "public-sub-2" {
-  vpc_id                  = aws_vpc.recipe.id
-  cidr_block              = "10.0.3.0/24"
-  availability_zone       = "ap-southeast-2b"
-  map_public_ip_on_launch = var.map_public_ip_on_launch
-}
+# resource "aws_subnet" "public-sub-2" {
+#   vpc_id                  = aws_vpc.recipe.id
+#   cidr_block              = "10.0.3.0/24"
+#   availability_zone       = "ap-southeast-2b"
+#   map_public_ip_on_launch = var.map_public_ip_on_launch
+# }
 
-resource "aws_subnet" "private-sub-1" {
-  vpc_id            = aws_vpc.recipe.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "ap-southeast-2a"
-}
+# resource "aws_subnet" "private-sub-1" {
+#   vpc_id            = aws_vpc.recipe.id
+#   cidr_block        = "10.0.2.0/24"
+#   availability_zone = "ap-southeast-2a"
+# }
 
-resource "aws_subnet" "private-sub-2" {
-  vpc_id            = aws_vpc.recipe.id
-  cidr_block        = "10.0.4.0/24"
-  availability_zone = "ap-southeast-2b"
-}
-
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.recipe.id
-
-  tags = {
-    Name = "main"
-  }
-}
-
-resource "aws_eip" "elas_ip" {
-  domain     = "vpc"
-  depends_on = [aws_internet_gateway.gw]
-}
-
-resource "aws_nat_gateway" "nat_gw" {
-  allocation_id = aws_eip.elas_ip.id
-  subnet_id     = aws_subnet.public-sub-1.id
-
-  tags = {
-    Name = "gw NAT"
-  }
-  depends_on = [aws_eip.elas_ip]
-}
+# resource "aws_subnet" "private-sub-2" {
+#   vpc_id            = aws_vpc.recipe.id
+#   cidr_block        = "10.0.4.0/24"
+#   availability_zone = "ap-southeast-2b"
+# }
 
 
-resource "aws_route_table" "pub_rte_tbl" {
-  vpc_id = aws_vpc.recipe.id
+# resource "aws_internet_gateway" "gw" {
+#   vpc_id = aws_vpc.recipe.id
+
+#   tags = {
+#     Name = "main"
+#   }
+# }
+
+# resource "aws_eip" "elas_ip" {
+#   domain     = "vpc"
+#   depends_on = [aws_internet_gateway.gw]
+# }
+
+# resource "aws_nat_gateway" "nat_gw" {
+#   allocation_id = aws_eip.elas_ip.id
+#   subnet_id     = aws_subnet.public-sub-1.id
+
+#   tags = {
+#     Name = "gw NAT"
+#   }
+#   depends_on = [aws_eip.elas_ip]
+# }
 
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
+# resource "aws_route_table" "pub_rte_tbl" {
+#   vpc_id = aws_vpc.recipe.id
 
-  tags = {
-    Name = "route_table_1"
-  }
-}
 
-resource "aws_route_table" "prv_rte_tbl" {
-  vpc_id = aws_vpc.recipe.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.gw.id
+#   }
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat_gw.id
-  }
+#   tags = {
+#     Name = "route_table_1"
+#   }
+# }
 
-  tags = {
-    Name = "route_table_2"
-  }
-}
+# resource "aws_route_table" "prv_rte_tbl" {
+#   vpc_id = aws_vpc.recipe.id
 
-resource "aws_route_table_association" "route_table_associations" {
-  for_each = {
-    private_subnet_1 = {
-      subnet_id      = aws_subnet.private-sub-1.id
-      route_table_id = aws_route_table.prv_rte_tbl.id
-    }
-    private_subnet_2 = {
-      subnet_id      = aws_subnet.private-sub-2.id
-      route_table_id = aws_route_table.prv_rte_tbl.id
-    }
-    public_subnet_1 = {
-      subnet_id      = aws_subnet.public-sub-1.id
-      route_table_id = aws_route_table.pub_rte_tbl.id
-    }
-    public_subnet_2 = {
-      subnet_id      = aws_subnet.public-sub-2.id
-      route_table_id = aws_route_table.pub_rte_tbl.id
-    }
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_nat_gateway.nat_gw.id
+#   }
 
-  subnet_id      = each.value.subnet_id
-  route_table_id = each.value.route_table_id
-} 
+#   tags = {
+#     Name = "route_table_2"
+#   }
+# }
+
+# resource "aws_route_table_association" "route_table_associations" {
+#   for_each = {
+#     private_subnet_1 = {
+#       subnet_id      = aws_subnet.private-sub-1.id
+#       route_table_id = aws_route_table.prv_rte_tbl.id
+#     }
+#     private_subnet_2 = {
+#       subnet_id      = aws_subnet.private-sub-2.id
+#       route_table_id = aws_route_table.prv_rte_tbl.id
+#     }
+#     public_subnet_1 = {
+#       subnet_id      = aws_subnet.public-sub-1.id
+#       route_table_id = aws_route_table.pub_rte_tbl.id
+#     }
+#     public_subnet_2 = {
+#       subnet_id      = aws_subnet.public-sub-2.id
+#       route_table_id = aws_route_table.pub_rte_tbl.id
+#     }
+#   }
+
+#   subnet_id      = each.value.subnet_id
+#   route_table_id = each.value.route_table_id
+# } 
 
