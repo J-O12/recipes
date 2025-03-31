2473b5bdb5532444afaf08f3dfda20292d0aca4e
J-O12 <joeloike@gmail.com>
2025-04-01 07:10:34 +1000

    recipe_web

--- Terraform/security-groups.tf
+++ Terraform/security-groups.tf
@@ -1,41 +1,41 @@
-resource "aws_security_group" "ALB_sec_group" {
-  vpc_id = aws_vpc.recipe.id
-  name   = "ALB"
+# resource "aws_security_group" "ALB_sec_group" {
+#   vpc_id = aws_vpc.recipe.id
+#   name   = "ALB"
 
-  ingress {
-    description      = "HTTPS"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+#   ingress {
+#     description      = "HTTPS"
+#     from_port        = 443
+#     to_port          = 443
+#     protocol         = "tcp"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
-resource "aws_security_group" "private" {
-  vpc_id = aws_vpc.recipe.id
-  name   = "private"
-  ingress {
-    description     = "HTTP"
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.ALB_sec_group.id]
-  }
+# resource "aws_security_group" "private" {
+#   vpc_id = aws_vpc.recipe.id
+#   name   = "private"
+#   ingress {
+#     description     = "HTTP"
+#     from_port       = 80
+#     to_port         = 80
+#     protocol        = "tcp"
+#     security_groups = [aws_security_group.ALB_sec_group.id]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }  
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }  
+# }
 
 
